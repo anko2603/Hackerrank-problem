95841cba7e7d31bb76bfebcc8cd0af3d4258abd8
anko2603 <122176710+anko2603@users.noreply.github.com>
2023-04-23 12:13:04 +0530

    Update plus-minus.py

--- plus-minus.py
+++ plus-minus.py
@@ -1,4 +1,5 @@
-#Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with 6 places after the decimal.
+# Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each 
+# fraction on a new line with 6 places after the decimal.
 import math
 import os
 import random
