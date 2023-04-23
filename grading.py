ddf6e319546247e62174a78bdc44f4bd20689153
anko2603 <122176710+anko2603@users.noreply.github.com>
2023-04-23 12:12:04 +0530

    Update grading.py

--- grading.py
+++ grading.py
@@ -1,4 +1,7 @@
-#HackerLand University has the following grading policy: Every student receives a grade in the inclusive range from 0 to 100.Any grade less than 40 is a failing grade.Sam is a professor at the university and likes to round each student's grade according to these rules:If the difference between the grade and the next multiple of 5 is less than 3, round grade up to the next multiple of 5.If the value of grade  is less than 38, no rounding occurs as the result will still be a failing grade.
+# HackerLand University has the following grading policy: Every student receives a grade in the inclusive range from 0 to 100.Any grade less
+# than 40 is a failing grade.Sam is a professor at the university and likes to round each student's grade according to these rules:If the 
+# difference between the grade and the next multiple of 5 is less than 3, round grade up to the next multiple of 5.If the value of grade  is 
+# less than 38, no rounding occurs as the result will still be a failing grade.
 import math
 import os
 import random
@@ -19,7 +22,7 @@ def gradingStudents(grades):
     return ans
 
 if __name__ == '__main__':
-    output_path = os.environ.get('OUTPUT_PATH', '/home/ankita/Desktop/Hackerrank-problem/output_file')
+    output_path = os.environ.get('OUTPUT_PATH', 'output.text')
     fptr = open(output_path, 'w')
 
     grades_count = int(input().strip())
