0e5accbad58c990ec8e8ee231eab1317a446dbca
anko2603 <122176710+anko2603@users.noreply.github.com>
2023-04-23 18:58:31 +0530

    Update mars.py

--- mars.py
+++ mars.py
@@ -1,4 +1,4 @@
-#A space explorer's ship crashed on Mars! They send a series of SOS messages to Earth for help.Letters in some of the SOS messages are altered by cosmic radiation during transmission. Given the signal received by Earth as a string,s, determine how many letters of the SOS message have been changed by radiation.
+# 
 import math
 import os
 import random
@@ -13,13 +13,9 @@ def marsExploration(s):
     for i,j in zip (s,target):
         if i != j:
           count+=1
-    return count  
-            
-        
-    
-    
+    return count 
 if __name__ == '__main__':
-    output_path = os.environ.get('OUTPUT_PATH', '/home/ankita/Desktop/Hackerrank-problem/output_file')
+    output_path = os.environ.get('OUTPUT_PATH', 'output.text')
     fptr = open(output_path, 'w')
 
     s = input()
