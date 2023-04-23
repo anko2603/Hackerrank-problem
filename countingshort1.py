c2755b68507b08290a58e181cd4a6ca787203f23
anko2603 <122176710+anko2603@users.noreply.github.com>
2023-04-23 12:06:07 +0530

    Update countingshort1.py

--- countingshort1.py
+++ countingshort1.py
@@ -1,4 +1,6 @@
-#Quicksort usually has a running time of n*log(n), but is there an algorithm that can sort even faster? In general, this is not possible. Most sorting algorithms are comparison sorts, i.e. they sort a list just by comparing the elements to one another. A comparison sort algorithm cannot beat n*log(n)(worst-case) running time, since n*log(n)represents the minimum number of comparisons needed to know where to place each element.
+# Quicksort usually has a running time of n*log(n), but is there an algorithm that can sort even faster? In general, this is not possible. 
+# Most sorting algorithms are comparison sorts, i.e. they sort a list just by comparing the elements to one another. A comparison sort algorithm 
+# cannot beat n*log(n)(worst-case) running time, since n*log(n)represents the minimum number of comparisons needed to know where to place each element.
 import math
 import os
 import random
@@ -13,7 +15,7 @@ def countingSort(arr):
     return count    
 
 if __name__ == '__main__':
-    output_path = os.environ.get('OUTPUT_PATH', '/home/ankita/Desktop/Hackerrank-problem/output_file')
+    output_path = os.environ.get('OUTPUT_PATH', 'output.text')
     fptr = open(output_path, 'w')
 
     n = int(input().strip())
