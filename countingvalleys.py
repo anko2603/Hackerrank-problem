0f43515e4c63cf6962121f46ca528f3947ac04cc
anko2603 <122176710+anko2603@users.noreply.github.com>
2023-04-23 12:07:38 +0530

    Update countingvalleys.py

--- countingvalleys.py
+++ countingvalleys.py
@@ -1,4 +1,8 @@
-#An avid hiker keeps meticulous records of their hikes. During the last hike that took exactly steps steps, for every step it was noted if it was an uphill,U , or a downhill,Dstep. Hikes always start and end at sea level, and each step up or down represents a 1 unit change in altitude. We define the following terms:A mountain is a sequence of consecutive steps above sea level, starting with a step up from sea level and ending with a step down to sea level.A valley is a sequence of consecutive steps below sea level, starting with a step down from sea level and ending with a step up to sea level.Given the sequence of up and down steps during a hike, find and print the number of valleys walked through.
+# An avid hiker keeps meticulous records of their hikes. During the last hike that took exactly steps steps, for every step it was noted if 
+# it was an uphill,U , or a downhill,Dstep. Hikes always start and end at sea level, and each step up or down represents a 1 unit change in altitude.
+# We define the following terms:A mountain is a sequence of consecutive steps above sea level, starting with a step up from sea level and ending with 
+# a step down to sea level.A valley is a sequence of consecutive steps below sea level, starting with a step down from sea level and ending with a
+# step up to sea level.Given the sequence of up and down steps during a hike, find and print the number of valleys walked through.
 import math
 import os
 import random
@@ -15,7 +19,7 @@ def countingValleys(steps, path):
             valleycount+=1
     return valleycount
 if __name__ == '__main__':
-    output_path = os.environ.get('OUTPUT_PATH', '/home/ankita/Desktop/Hackerrank-problem/output_file')
+    output_path = os.environ.get('OUTPUT_PATH', 'output.text')
     fptr = open(output_path, 'w')
 
     steps = int(input().strip())
