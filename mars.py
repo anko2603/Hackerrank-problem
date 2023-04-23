150e89e071c9536d112e7e354d94deba08e9c243
anko2603 <122176710+anko2603@users.noreply.github.com>
2023-04-23 19:00:18 +0530

    Update mars.py

--- mars.py
+++ mars.py
@@ -1,4 +1,6 @@
-# 
+# A space explorer's ship crashed on Mars! They send a series of SOS messages to Earth for help.Letters in some of the 
+# SOS messages are altered by cosmic radiation during transmission. Given the signal received by Earth as a string,s, determine
+# how many letters of the SOS message have been changed by radiation.
 import math
 import os
 import random
