cab76e47d167eb00caa0acc5278cfeaadaf96d80
anko2603 <122176710+anko2603@users.noreply.github.com>
2023-04-23 12:04:32 +0530

    Update camelcase.py

--- camelcase.py
+++ camelcase.py
@@ -1,4 +1,7 @@
-#Camel Case is a naming style common in many programming languages. In Java, method and variable names typically start with a lowercase letter, with all subsequen words starting with a capital letter (example: startThread). Names of classes follow the same pattern, except that they start with a capital letter (example: BlueCar).Your task is to write a program that creates or splits Camel Case variable, method, and class names.
+# Camel Case is a naming style common in many programming languages. In Java, method and variable names typically start with a lowercase letter, with 
+# all subsequen words starting with a capital letter (example: startThread). Names of classes follow the same pattern, except that they start with 
+# a capital letter (example: BlueCar).Your task is to write a program that creates or splits
+# Camel Case variable, method, and class names.
 while True:
     try:
         q = input().rstrip()
