d3ec6c3f12bd57c188629ee527c5d3dc8c7d8c95
anko2603 <122176710+anko2603@users.noreply.github.com>
2023-04-27 00:08:31 +0530

    Update zigzag.py

--- zigzag.py
+++ zigzag.py
@@ -1,4 +1,7 @@
-#In this challenge, the task is to debug the existing code to successfully execute all provided test files.Given an array of n distinct integers, transform the array into a zig zag sequence by permuting the array elements. A sequence will be called a zig zag sequence if the first k elements in the sequence are in increasing order and the last k  elements are in decreasing order, where k=(n+1)/2 . You need to find the lexicographically smallest zig zag sequence of the given array.
+#In this challenge, the task is to debug the existing code to successfully execute all provided test files.Given an array 
+# of n distinct integers, transform the array into a zig zag sequence by permuting the array elements. A sequence will be 
+# called a zig zag sequence if the first k elements in the sequence are in increasing order and the last k  elements are in 
+# decreasing order, where k=(n+1)/2 . You need to find the lexicographically smallest zig zag sequence of the given array.
 def findZigZagSequence(a, n):
     a.sort()
     mid = int((n + 1)/2)-1
