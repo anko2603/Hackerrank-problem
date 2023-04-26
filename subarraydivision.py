2ac155be83a3b2c15ea83bffc2e7cbbc534b1341
anko2603 <122176710+anko2603@users.noreply.github.com>
2023-04-26 10:57:13 +0530

    Update subarraydivision.py

--- subarraydivision.py
+++ subarraydivision.py
@@ -7,3 +7,34 @@ import random
 import re
 import sys
 
+def birthday(s, d, m):
+    count=0
+    total=sum(s[:m])
+    if total ==d:
+        count+=1
+    for i in range(m,len(s)):
+        total+=s[i]
+        total-=s[i-m]
+        if total ==d:
+            count+=1
+    return count
+
+if __name__ == '__main__':
+    output_path = os.environ.get('OUTPUT_PATH', 'output.text')
+    fptr = open(output_path, 'w')
+    
+    n = int(input().strip())
+
+    s = list(map(int, input().rstrip().split()))
+
+    first_multiple_input = input().rstrip().split()
+
+    d = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    result = birthday(s, d, m)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
